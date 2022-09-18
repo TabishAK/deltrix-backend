237b307bb786e2078a40ca22c3d9b3fdf670ff35
tabishak <tabish@geeksofkolachi.com>
2022-09-19 02:05:27 +0500

    Mongo URI changed

--- index.js
+++ index.js
@@ -1,36 +1,39 @@
-var express = require("express")
-var bodyParser = require("body-parser")
+var express = require("express");
+var bodyParser = require("body-parser");
 var cors = require("cors");
-var app = express()
-var mongoose = require("mongoose")
+var app = express();
+var mongoose = require("mongoose");
 
-mongoose.set('useCreateIndex', true);
-mongoose.connect("mongodb://localhost:27017/apptesting", { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.set("useCreateIndex", true);
+mongoose.connect(
+  "mongodb+srv://deltrix:deltrix786@cluster0.kdziyzt.mongodb.net/?retryWrites=true&w=majority",
+  { useNewUrlParser: true, useUnifiedTopology: true }
+);
 
-const account = require("./app/routes/accounts")
-const adminAccount = require("./app/routes/adminAuth")
-const adminCreatedUse = require("./app/routes/adminCreatedUse")
-const category = require("./app/routes/category")
-const product = require("./app/routes/Product")
-const order = require("./app/routes/Order")
-const homeHelp = require("./app/routes/homeHelp")
-const slasemanHelp = require("./app/routes/SlasenameHelp")
-const marcketingHelp = require("./app/routes/marketingHelp")
-const DashbordImages = require("./app/routes/DashbordImages")
-const upload = require("./app/routes/Upload")
-const discount = require("./app/routes/Discount")
-const banckDetails = require("./app/routes/banckDetails")
-const facebookAuth = require("./app/routes/facebookAuth")
-const payment = require("./app/routes/Payment")
-const notification = require("./app/routes/Notification")
-const wishList = require("./app/routes/wishList")
+const account = require("./app/routes/accounts");
+const adminAccount = require("./app/routes/adminAuth");
+const adminCreatedUse = require("./app/routes/adminCreatedUse");
+const category = require("./app/routes/category");
+const product = require("./app/routes/Product");
+const order = require("./app/routes/Order");
+const homeHelp = require("./app/routes/homeHelp");
+const slasemanHelp = require("./app/routes/SlasenameHelp");
+const marcketingHelp = require("./app/routes/marketingHelp");
+const DashbordImages = require("./app/routes/DashbordImages");
+const upload = require("./app/routes/Upload");
+const discount = require("./app/routes/Discount");
+const banckDetails = require("./app/routes/banckDetails");
+const facebookAuth = require("./app/routes/facebookAuth");
+const payment = require("./app/routes/Payment");
+const notification = require("./app/routes/Notification");
+const wishList = require("./app/routes/wishList");
 // ===============================================
 // ===============================================
 // ===============================================
-app.use(cors())
-app.set("port", process.env.PORT || 8000)
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(bodyParser.json({ limit: "5000kb" }))
+app.use(cors());
+app.set("port", process.env.PORT || 8000);
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json({ limit: "5000kb" }));
 
 app.use("/account", account);
 app.use("/adminauth", adminAccount);
@@ -50,5 +53,6 @@ app.use("/payment", payment);
 app.use("/firebase", notification);
 app.use("/wishList", wishList);
 
-
-app.listen(app.get("port"), () => console.log("Server is running on port 8000"))
+app.listen(app.get("port"), () =>
+  console.log("Server is running on port 8000")
+);
